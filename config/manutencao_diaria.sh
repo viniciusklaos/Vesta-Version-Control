df811662a523def698805ec8b01a2d5a1eee8cf2
KlaosVK <vitalhost11@gmail.com>
2018-04-09 13:09:41 -0300

    Initial commit

---
+++ config/manutencao_diaria.sh
@@ -0,0 +1,188 @@
+# (env) PATH:
+
+# ----  Variáveis globais  ---- #
+
+PATH=$PATH
+DIAGERAL=`date | awk '{print $3}'`
+MESGERAL=`date | awk '{print $2}'`
+DATAGERAL="$DIAGERAL de $MESGERAL"
+
+
+# --------- Funcoes ----------- #
+
+limparexim (){
+	find /var/spool/exim/input -type f | xargs rm -f {}/;
+	echo "EXIM atingiu 25GB e foi limpo tudo - $DATAGERAL" >> /var/log/manutencao.log
+}
+
+limparpmta (){
+	find /var/spool/pmta -type f | xargs rm -f {}/;
+	echo "PMTA atingiu 15GB e foi limpo tudo - $DATAGERAL" >> /var/log/manutencao.log
+}
+
+processar_spool_pmta (){
+	find /var/spool/pmta -type f -daystart -mtime +3.5 | xargs rm -f {}/;
+	echo "Limpeza PMTA-SPOOL - $DATAGERAL" >> /var/log/manutencao.log
+}
+
+processar_spool_exim (){
+	find /var/spool/exim/input -type f -daystart -mtime +3.5 | xargs rm -f {}/;
+	echo "Limpeza EXIM-SPOOL - $DATAGERAL" >> /var/log/manutencao.log
+}
+
+processar_logs_pmta (){
+	# Criando o diretorio para salvar os LOGS separados
+	
+	[ -d /home/vtinstall ] || mkdir /home/vtinstall/; cd /home/vtinstall
+	[ -d /home/vtinstall/PMTA-Logs ] || mkdir /home/vtinstall/PMTA-Logs; cd /home/vtinstall/PMTA-Logs
+	
+	# Processo de separacao de LOGS
+	
+	for logs in `find /var/log/pmta/ -type f -name "acct*.csv" -print`; do 
+		sed '/4.4.1/!d' $logs | cut -d, -f 5 | awk '!a[$0]++' | sort | uniq >> /home/vtinstall/PMTA-Logs/Status_indefinido.txt;
+		sed '/5.0.0/!d' $logs | cut -d, -f 5 | awk '!a[$0]++' | sort | uniq >> /home/vtinstall/PMTA-Logs/Status_indefinido.txt;
+		sed '/5.2.1/!d' $logs | cut -d, -f 5 | awk '!a[$0]++' | sort | uniq >> /home/vtinstall/PMTA-Logs/Invalido.txt;
+		sed '/5.3.0/!d' $logs | cut -d, -f 5 | awk '!a[$0]++' | sort | uniq >> /home/vtinstall/PMTA-Logs/Invalido.txt;
+		sed '/5.4.4/!d' $logs | cut -d, -f 5 | awk '!a[$0]++' | sort | uniq >> /home/vtinstall/PMTA-Logs/Invalido.txt;
+		sed '/5.1.2/!d' $logs | cut -d, -f 5 | awk '!a[$0]++' | sort | uniq >> /home/vtinstall/PMTA-Logs/Invalido.txt;
+		sed '/5.1.2/!d' $logs | cut -d, -f 5 | awk '!a[$0]++' | sort | uniq >> /home/vtinstall/PMTA-Logs/Invalido.txt;
+		sed '/5.1.1/!d' $logs | cut -d, -f 5 | awk '!a[$0]++' | sort | uniq >> /home/vtinstall/PMTA-Logs/Invalido.txt;
+		sed '/5.3.2/!d' $logs | cut -d, -f 5 | awk '!a[$0]++' | sort | uniq >> /home/vtinstall/PMTA-Logs/Mensagem_Recusada.txt;
+		sed '/5.7.1/!d' $logs | cut -d, -f 5 | awk '!a[$0]++' | sort | uniq >> /home/vtinstall/PMTA-Logs/Mensagem_Recusada.txt;
+		sed '/5.4.6/!d' $logs | cut -d, -f 5 | awk '!a[$0]++' | sort | uniq >> /home/vtinstall/PMTA-Logs/Mensagem_Recusada.txt;
+		sed '/2.0.0/!d' $logs | cut -d, -f 5 | awk '!a[$0]++' | sort | uniq >> /home/vtinstall/PMTA-Logs/Sucesso.txt;
+		sed '/5.2.2/!d' $logs | cut -d, -f 5 | awk '!a[$0]++' | sort | uniq >> /home/vtinstall/PMTA-Logs/Caixa_cheia.txt;
+	done
+	
+	# Criando novos temporarios
+	
+	cat /home/vtinstall/PMTA-Logs/Status_indefinido.txt | sort | uniq > /home/vtinstall/PMTA-Logs/Status_indefinido.tmp
+	cat /home/vtinstall/PMTA-Logs/Invalido.txt | sort | uniq > /home/vtinstall/PMTA-Logs/Invalido.tmp
+	cat /home/vtinstall/PMTA-Logs/Mensagem_Recusada.txt | sort | uniq > /home/vtinstall/PMTA-Logs/Mensagem_Recusada.tmp
+	cat /home/vtinstall/PMTA-Logs/Caixa_cheia.txt | sort | uniq > /home/vtinstall/PMTA-Logs/Caixa_cheia.tmp
+	cat /home/vtinstall/PMTA-Logs/Sucesso.txt | sort | uniq > /home/vtinstall/PMTA-Logs/Sucesso.tmp
+	
+	# Trazendo temporarios para original
+	
+	cat /home/vtinstall/PMTA-Logs/Status_indefinido.tmp > /home/vtinstall/PMTA-Logs/Status_indefinido.txt
+	cat /home/vtinstall/PMTA-Logs/Invalido.tmp > /home/vtinstall/PMTA-Logs/Invalido.txt
+	cat /home/vtinstall/PMTA-Logs/Mensagem_Recusada.tmp > /home/vtinstall/PMTA-Logs/Mensagem_Recusada.txt
+	cat /home/vtinstall/PMTA-Logs/Caixa_cheia.tmp > /home/vtinstall/PMTA-Logs/Caixa_cheia.txt
+	cat /home/vtinstall/PMTA-Logs/Sucesso.tmp > /home/vtinstall/PMTA-Logs/Sucesso.txt
+	
+	# Apagando arquivos temporarios
+	
+	rm -f /home/vtinstall/PMTA-Logs/Status_indefinido.tmp
+	rm -f /home/vtinstall/PMTA-Logs/Invalido.tmp
+	rm -f /home/vtinstall/PMTA-Logs/Mensagem_Recusada.tmp
+	rm -f /home/vtinstall/PMTA-Logs/Caixa_cheia.tmp
+	rm -f /home/vtinstall/PMTA-Logs/Sucesso.tmp
+	
+	
+	# Limpando filas SE NAO TIVER ENVIOS
+	
+	QUEUE=`pmta show status | grep SMTP | awk '{print $2}'`
+	
+	if [ $QUEUE -eq 0 ]; then
+		
+		service pmta stop
+		service exim stop
+	
+		rm -fr /var/spool/pmta/
+		mkdir /var/spool/pmta/
+		chmod 770 /var/spool/pmta/ -R
+		chown pmta.pmta /var/spool/pmta/ -R
+	
+		rm -fr /var/spool/exim
+		mkdir /var/spool/exim
+		chmod 750 /var/spool/exim -R
+		chown exim.exim /var/spool/exim -R
+	
+		rm -fr /var/log/pmta/
+		mkdir /var/log/pmta/
+		chmod 770 /var/log/pmta/ -R
+		chown pmta. /var/log/pmta/ -R
+	
+		rm -fr /var/log/exim/
+		mkdir /var/log/exim/
+		chmod 750 /var/log/exim/ -R
+		chown exim.exim /var/log/exim/ -R
+	
+		service pmta start
+		service exim start
+	
+		pmta clear dnscache domain
+	fi
+	echo "Processado LOGs do PMTA - $DATAGERAL" >> /var/log/manutencao.log
+}
+
+processar_logs_exim () {
+	cd /var/log/exim
+	
+	DIA=`date | awk '{print $3}'`
+	MES=`date | awk '{print $2}'`
+	DATA="$DIA-$MES"
+	
+	array=("main" "panic" "reject")
+	
+	for LOG in ${array[@]}; do
+		if [ -f ${LOG}.log-* ]; then
+			tar -cf ${LOG}_log_$DATA.tar ${LOG}.log-*
+			rm -f ${LOG}.log-*;
+		fi
+	done;
+	echo "Processado LOGs do EXIM - $DATAGERAL" >> /var/log/manutencao.log
+}
+
+processar_backups (){
+	find /home/backup/ -name "admin.*" | xargs rm -f {}/;
+	find /home/backup/ -name "*.tar" -daystart -mtime +0.5 | xargs rm -f {}/;
+	echo "Limpeza da pasta backup - $DATAGERAL" >> /var/log/manutencao.log
+}
+
+processar_emails (){
+	for dominio in `v-list-users | tail -n +3 | awk '{print "v-list-web-domains "$1" | tail -n +3"}' | bash | awk '{print $1}' | sed '/vesta./d'`; do
+		for conta in `find /home/*/web -type d -name $dominio | cut -d/ -f3`; do
+        	for email in `find /home/$conta/mail/$dominio/ -type d  | cut -d/ -f6 | uniq | tail -n +2`; do
+        	    find /home/$conta/mail/$dominio/$email/new/ -type f -daystart -mtime +2.5 | xargs rm -f {}/;
+        	    echo "Limpeza : /home/$conta/mail/$dominio/$email/new/ - $DATAGERAL"
+        	done;
+    	done;
+	done;
+}
+
+
+
+# Higienizacao do PMTA #
+if [ -f /etc/pmta/config ]; then
+	processar_logs_pmta;
+	processar_spool_pmta;
+	PMTATAMANHO=`du -sh /var/spool/pmta | awk '{print $1}'`
+	echo "$PMTATAMANHO" | grep -q G && PMTATAMANHO=`echo "$PMTATAMANHO" | cut -d'G' -f1` || sleep 0;
+	echo "$PMTATAMANHO" | grep -q M && PMTATAMANHO=`echo "$PMTATAMANHO" | cut -d'M' -f1` || sleep 0;
+	echo "$PMTATAMANHO" | grep -q K && PMTATAMANHO=`echo "$PMTATAMANHO" | cut -d'K' -f1` || sleep 0;
+	PMTATAMANHO=`echo $PMTATAMANHO | bc | cut -d. -f1`
+	if [ $PMTATAMANHO -ge "15" ]; then
+        limparpmta;
+	fi
+fi
+
+# Higienizacao do EXIM #
+processar_logs_exim;
+processar_spool_exim;
+
+EXIMTAMANHO=`du -sh /var/spool/exim/input | awk '{print $1}'`
+echo "$EXIMTAMANHO" | grep -q G && EXIMTAMANHO=`echo "$EXIMTAMANHO" | cut -d'G' -f1` || sleep 0;
+echo "$EXIMTAMANHO" | grep -q M && EXIMTAMANHO=`echo "$EXIMTAMANHO" | cut -d'M' -f1` || sleep 0;
+echo "$EXIMTAMANHO" | grep -q K && EXIMTAMANHO=`echo "$EXIMTAMANHO" | cut -d'K' -f1` || sleep 0;
+EXIMTAMANHO=`echo $EXIMTAMANHO | bc | cut -d. -f1`
+
+if [ $EXIMTAMANHO -ge "25" ]; then
+        limparexim;
+fi
+
+# Higienizacao dos backups #
+processar_backups;
+
+# Higienizacao dos e-mails #
+processar_emails;
---
+++ pre-install.sh
@@ -0,0 +1,175 @@
+cd /tmp
+DOMAIN=`hostname | cut -d. -f2-100`
+IPPRINCIPAL=`ip a | grep 'inet ' | grep -v 127.0.0.1 | awk '{print $2}' | cut -f1 -d/ | grep -n ^ | grep -w ^1 | cut -f2 -d:`
+# Baixando e instalando o VESTACP
+curl -O http://vestacp.com/pub/vst-install.sh
+echo y | bash vst-install.sh --nginx no --apache yes --phpfpm no --named yes --remi no --vsftpd yes --proftpd no --iptables no --fail2ban no --quota no --exim yes --dovecot yes --spamassassin no --clamav no --softaculous no --mysql yes --postgresql no --hostname vesta.$DOMAIN --email contato@$DOMAIN --password phCA24bgK --force
+# Permissoes para o WEBMAIL funcionar
+chmod 777 /etc/roundcubemail/defaults.inc.php
+chmod 777 /etc/roundcubemail/config.inc.php
+# Criacao do MAILIPS, MAILHELO e EXIM.PL
+> /etc/mailips
+> /etc/mailhelo
+> /etc/exim/exim.pl
+# PUBLIC_HTML padrao
+if [ -d /usr/local/vesta/data/templates/web/skel/public_html ]; then
+  cd /usr/local/vesta/data/templates/web/skel/public_html/
+  rm -Rf *
+  curl -O http://rep.vitalhost.com.br/v4/semcpanel/public.tar.gz
+  tar -vxzf public.tar.gz
+  rm -Rf public.tar.gz
+  cd /tmp
+fi
+# Criacao das pastas VT
+[ -d /home/vtinstall/vartemp ] || mkdir -p /home/vtinstall/vartemp;
+[ -d /home/vtinstall/scripts ] || mkdir -p /home/vtinstall/scripts;
+echo "$DOMAIN" > /home/vtinstall/vartemp/dominiobase
+#
+# Download dos packs do VESTA
+curl -O http://rep.vitalhost.com.br/v4/vestacp/pack.zip
+unzip -q pack.zip
+#
+cat remover > /usr/local/bin/remover
+cat dedicar > /usr/local/bin/dedicar
+cat redirect > /usr/local/bin/redirect
+cat ipuso > /usr/local/bin/ipuso
+cat instalar > /usr/local/bin/instalar
+cat dominios > /usr/local/bin/dominios
+cat ips > /usr/local/bin/ips
+cat status > /usr/local/bin/status
+cat res_backup_cp > /usr/local/bin/res_backup_cp
+cat res_backup_cp_start > /usr/local/bin/res_backup_cp_start
+cat suspender > /usr/local/bin/suspender
+cat unsuspender > /usr/local/bin/unsuspender
+cat installpmta > /usr/local/bin/installpmta
+#
+# Permissoes globais
+chmod a+x /usr/local/bin/ipuso
+chmod a+x /usr/local/bin/instalar
+chmod a+x /usr/local/bin/dominios
+chmod a+x /usr/local/bin/ips
+chmod a+x /usr/local/bin/status
+chmod a+x /usr/local/bin/redirect
+chmod a+x /usr/local/bin/dedicar
+chmod a+x /usr/local/bin/remover
+chmod a+x /usr/local/bin/res_backup_cp
+chmod a+x /usr/local/bin/res_backup_cp_start
+chmod a+x /usr/local/bin/unsuspender
+chmod a+x /usr/local/bin/suspender
+chmod a+x /usr/local/bin/installpmta
+#
+# Download dos configs do VESTA
+curl -O http://rep.vitalhost.com.br/v4/vestacp/config.zip
+unzip -q config.zip
+#
+cat php.ini > /etc/php.ini
+cat exim.conf > /etc/exim/exim.conf
+cat manutencao_diaria.sh > /home/vtinstall/scripts/manutencao_diaria.sh
+#
+# Cron de gerenciamento
+echo "0 0 */1 * * sh /home/vtinstall/scripts/manutencao_diaria.sh" >> /var/spool/cron/root
+#
+# Fallback hosts do EXIM
+sed -i "s/fallback_hosts = vesta.cartadeouro.com.br/fallback_hosts = vesta.$DOMAIN/g" /etc/exim/exim.conf
+sed -i "s/IPPRINCIPAL/$IPPRINCIPAL/g" /etc/exim/exim.conf
+#
+# Processamento EXIM
+sed -i "s/QUEUE=1h/QUEUE=3m/g" /etc/init.d/exim
+#
+# Dados FTP
+echo "tv2.revendabrasil.com.br" > /home/vtinstall/vartemp/hostftp.info
+echo "backupvp" > /home/vtinstall/vartemp/usuarioftp.info
+echo "phCA24bgK" > /home/vtinstall/vartemp/senhaftp.info
+#
+# MOTD
+echo "" > /etc/motd
+echo "Bem vindo(a)" >> /etc/motd
+echo "Servidor de plataforma VestaCP" >> /etc/motd
+echo "" >> /etc/motd
+#
+# Ajutes de memoria RAM para o MySQL
+if ! grep -q innodb_buffer_pool_size /etc/my.cnf; then
+  RAM=`free -m | grep Mem: | awk '{print $2}'`
+  POOL=`echo "scale=2; (($RAM / 100 * 25) * 1024 * 1024) " | bc | cut -d. -f1`
+  sed -i "2s/^/innodb_buffer_pool_size = $POOL\n/" /etc/my.cnf
+fi
+#
+sed -i '/^#/d' /etc/my.cnf
+sed -i '/max_connections/d' /etc/my.cnf
+sed -i '/max_user_connections/d' /etc/my.cnf
+sed -i '/wait_timeout/d' /etc/my.cnf
+sed -i '/interactive_timeout/d' /etc/my.cnf
+sed -i '/long_query_time/d' /etc/my.cnf
+sed -i '/symbolic-links/d' /etc/my.cnf
+#
+# Pagina de suspensao personalizada com chat:
+echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">
+<html>
+<head>
+<title>Conta Suspensa!!</title>
+<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\">
+</head>
+<body bgcolor=\"#CCCCCC\">
+<center>
+<table width=\"633\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" bgcolor=\"#FFFFFF\">
+<!--DWLayoutTable-->
+<tr>
+  <td width=\"23\" height=\"17\"></td>
+  <td width=\"590\"></td>
+  <td width=\"20\"></td>
+</tr>
+<tr>
+  <td height=\"310\"></td>
+  <td valign=\"top\"><p align=\"center\">&nbsp;</p>
+    <p align=\"center\"><font size=\"4\" face=\"verdana\"><strong>Sua conta de Email Marketing est&aacute;
+      suspensa por motivos administrativos!</strong></font></p>
+    <p align=\"center\"><font size=\"2\" face=\"verdana\">Para ativ&aacute;-la novamente
+      ou at&eacute; mesmo verificar o que houve, entre em contato conosco
+      atrav&eacute;s dos telefones abaixo: </font></p>
+    <p><font size=\"2\" face=\"verdana\"> (31) 2565 9120 - Belo Horizonte <br>
+      (31) 4063 9358 - Belo Horizonte<br>
+      (11) 4063 6505 - SP Voip<br>
+      (21) 4063 7590 - Rio de Janeiro<br>
+      (41) 4063 7742 - Curitiba</font></p>
+    <p><font size=\"2\" face=\"verdana\"><font color=\"#FF0000\" size=\"1\"><em>Segunda &agrave; Sexta-Feira
+      9:00 &aacute;s 18:00Hs - *exceto feriados</em></font></font></p>
+    <p align=\"center\"><font size=\"2\" face=\"verdana\"><em>Se preferir clique
+      no link abaixo para abrir um Ticket/Chamado.</em><br>
+      <a href=\"http://www.centraldousuario.com.br/submitticket.php?step=2&deptid=1\">[Abrir
+      Ticket]</a></font></p>
+
+       <p><font size=\"2\" face=\"verdana\"><font color=\"#FF0000\" size=\"1\"><em>Nosso CHAT ON-LINE est&aacute;  disponivel para melhor atende-lo no fim desta p&aacute;gina.</em></font></font></p>
+
+
+    </td>
+  <td></td>
+</tr>
+<tr>
+  <td height=\"33\"></td>
+  <td>&nbsp;</td>
+  <td></td>
+</tr>
+</table>
+</center>
+</body>
+<!--Start of Tawk.to Script-->
+<script type=\"text/javascript\">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement(\"script\"),s0=document.getElementsByTagName(\"script\")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5829e4487295ad7394c88221/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+</html>" > /usr/local/vesta/data/templates/web/suspend/index.html
+#
+service exim restart
+service mysqld restart
+service crond restart
+#
+rm -Rf *
+reboot
